d46874b0f61f9b36ea9b3942932ed44c1e22643a
Baratam Sai Harshitha <150455720+HARSHITHAbaratam@users.noreply.github.com>
2026-07-26 19:36:45 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l=0,r=1;
+        int prof=0;
+        while(r<prices.size()){
+            if(prices[r]-prices[l]>0){
+                prof=max(prof,prices[r]-prices[l]);
+            }else{
+                l=r;
+            }r++;
+        }return prof;
+    }
+};
